85d535556a06a338e96e339a01056a4d6d22bf41
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2025-05-24 09:02:54 +0100

    app setup

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());  // For parsing application/json
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+// Default route for health check
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Error handling middleware (catching unexpected errors)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`You can find the server on port ${PORT}`);
+});
